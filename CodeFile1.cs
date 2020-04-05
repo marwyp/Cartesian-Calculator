7ff4408676c329f2c6f46d78d220eb654e8af300
Marek <63198055+marwyp@users.noreply.github.com>
2020-04-05 20:41:40 +0200

    Add Program.cs, CodeFile1.cs

---
+++ CodeFile1.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Funkcje
+{ 
+    enum aktywne_dzialanie
+    {
+        red,
+        blue,
+        green,
+        yellow,
+        pink,
+        brown
+    }
+}
